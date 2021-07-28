5b9e68635ea18b6b0e48770d3b4012c9e876386c
JJCprodeveloper <jjcdeveloper@gmail.com>
2021-07-28 23:27:38 +0800

    Garbage Collector ConfigSettings variables update

--- src/me/Cutiemango/MangoQuest/ConfigSettings.java
+++ src/me/Cutiemango/MangoQuest/ConfigSettings.java
@@ -10,12 +10,15 @@ public class ConfigSettings
 	public static int MAXIMUM_QUEST_AMOUNT = 4;
 	public static int MAXIMUM_DISPLAY_QUEST_AMOUNT = 3;
 	public static int PLAYER_DATA_SAVE_INTERVAL = 600;
+	//SQL Garbage Collector in ticks
+	public static int SQL_CLEAR_INTERVAL = 24000;
 	public static int CONVERSATION_ACTION_INTERVAL_IN_TICKS = 25;
 
 	public static boolean POP_LOGIN_MESSAGE = true;
 	public static boolean ENABLE_SCOREBOARD = true;
 	public static boolean USE_PARTICLE_EFFECT = true;
 	public static boolean ENABLE_SKIP = false;
+	
 
 	public static boolean USE_WEAK_ITEM_CHECK = false;
 
